82a7933dd5d34b03486ef359cb86db8a8acf48b5
Jakub <jakub.jakubecky123@gmail.com>
2020-10-14 16:54:04 +0200

    c

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ class Connection{
     float weight;
     Neuron * inputNeuron;
     Neuron * outputNeuron;
+
+    bool chainSet = false;
+    float chain;
 public:
     Connection(Neuron * inputNeuron, Neuron * outputNeuron, float w){
         this->inputNeuron = inputNeuron;
@@ -24,6 +27,19 @@ public:
     }
     void setWeight(float w){
         this->weight = w;
+    }
+    void setChain(float chain){
+        this->chain = chain;
+        chainSet = true;
+    }
+    void resetChain(){
+        this->chainSet = false;
+    }
+    bool isChainSet(){
+        return chainSet;
+    }
+    float getChain(){
+        return chain;
     }
     float getWeight(){
         return weight;
@@ -44,7 +60,8 @@ class Neuron{
     float bias;
     int idInLayer;
     FFLayer * layer;
-    vector<Connection*> connections;
+    vector<Connection*> inputConnections;
+    vector<Connection*> outputConnections;
     float output;
     float netVal;
 
@@ -53,8 +70,11 @@ public:
         this->idInLayer = idInLayer;
         this->layer = l;
     }
-    void addConnection(Connection * c){
-        this->connections.push_back(c);
+    void addInputConnection(Connection * c){
+        this->inputConnections.push_back(c);
+    }
+    void addOutputConnection(Connection * c){
+        this->outputConnections.push_back(c);
     }
     void setLayer(FFLayer * l){
         this->layer = l;
@@ -62,16 +82,19 @@ public:
     FFLayer * getLayer(){
         return layer;
     }
-    vector<Connection*> getConnections(){
-        return connections;
+    vector<Connection*> getInputConnections(){
+        return inputConnections;
+    }
+    vector<Connection*> getOutputConnections(){
+        return outputConnections;
     }
     void setWeight(Connection *c){
-        this->connections.push_back(c);
+        this->inputConnections.push_back(c);
     }
     void countActivation(){
         float sum = 0;
-        for(int i = 0; i < connections.size(); i ++){
-            Connection *c = connections.at(i);
+        for(int i = 0; i < inputConnections.size(); i ++){
+            Connection *c = inputConnections.at(i);
             sum += (c->getInputNeuron()->getOutput() * c->getWeight());
         }
         this->netVal = sum;
@@ -83,6 +106,9 @@ public:
     }
     void setOutput(float o){
         this->output = o;
+    }
+    int getIndex(){
+        return idInLayer;
     }
     float getOutput(){
         return output;
@@ -122,6 +148,9 @@ public:
     }
     vector<Connection * > getConnList(){
         return connList;
+    }
+    void addToConnList(Connection *c){
+        this->connList.push_back(c);
     }
     Network(Network * p1, Network * p2){
         if(p1->ffLayers.size() == p2->ffLayers.size())
@@ -188,7 +217,9 @@ void FFLayer::initRandom(int neuronsCount) {
            for(int j = 0; j < prevV.size(); j++){
                Neuron * pn = prevV.at(j);
                Connection * c = new Connection(pn, n, net->random(0,1));
-                n->addConnection(c);
+               n->addInputConnection(c);
+               pn->addOutputConnection(c);
+               net->addToConnList(c);
            }
        }
     }
@@ -207,16 +238,31 @@ void FFLayer::run(){
     }
 }
 class BackProp{
-    const float learningRate = 1;
+    const float learningRate = 10000;
     Network * net;
 
 
 
-    float getChain(Connection *c){
+    float getChain(Connection *c, vector<float> expected) {
         float chain = 1;
         float act = sigmoid(c->getOutputNeuron()->getNetVal());
-        chain *= act*(1.f- act);
-        if(c->getOutputNeuron())
+        chain *= act * (1.f - act);
+        float tmp = 0;
+        if(c->isChainSet())
+            return c->getChain();
+
+        if (c->getOutputNeuron()->getLayer()->getIndexInNet() != net->getFFLayers().size() - 1) {
+            vector<Connection *> connList = c->getOutputNeuron()->getOutputConnections();
+            for (int i = 0; i < connList.size(); i++) {
+                Connection *nc = connList.at(i);
+                tmp += nc->getWeight() * getChain(nc, expected);
+            }
+            chain *= tmp;
+        } else{
+            chain *= 2*(c->getOutputNeuron()->getOutput() - expected.at(c->getOutputNeuron()->getIndex()));
+        }
+        c->setChain(chain);
+        //todo reset chains in neuron!!
         return chain;
     }
 
@@ -224,18 +270,20 @@ public:
     BackProp(Network * net){
         this->net = net;
     }
+    float getLoss(vector<float> expected){
+        vector<Neuron *> neurons = net->getFFLayers().at(net->getFFLayers().size()-1)->getNeurons();
+        float loss = 0;
+        for(int i = 0; i < neurons.size(); i++){
+            loss += pow(neurons.at(i)->getOutput() - expected.at(i), 2);
+        }
+        return loss;
+    }
     void learn(vector<float> expected){
         FFLayer * lastLayer = net->getFFLayers().at(net->getFFLayers().size()-1);
         for(int i = 0; i < net->getConnList().size(); i ++){
             Connection * c = net->getConnList().at(i);
-            float chain = c->getInputNeuron()->getOutput();
-            chain *= getChain(c);
-            if(c->getOutputNeuron()->getLayer()->getIndexInNet() != net->getFFLayers().size()-1){
-                FFLayer* nextLayer = net->getFFLayers().at(c->getOutputNeuron()->getLayer()->getIndexInNet()+1);
-                for(int j = 0; j < nextLayer->getNeurons().size(); j ++){
-
-                }
-            }
+            float delta = learningRate * c->getInputNeuron()->getOutput() * getChain(c, expected);
+            c->setWeight(c->getWeight() - delta);
         }
     }
 };
@@ -247,9 +295,11 @@ int main(){
     vector<float> input = {0.44, 0.55, 0.1, 0.6, 0.44, 0.55, 0.1, 0.6, 0.44, 0.55, 0.1, 0.6, 0.44, 0.55, 0.1, 0.6};
     network->run(input);
     vector<float> result = network->getResult();
-    for(int  i = 0; i < result.size(); i ++){
-        cout<<result.at(i);
+    vector<float> expected = {0.0f, 0.0f, 0.0f, 0.0f, 1.f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,0.0f, 0.0f,};
+    BackProp *backProp = new BackProp(network);
+    for(int i = 0; i < 100; i ++){
+        backProp->learn(expected);
+        cout << "Loss = " << backProp->getLoss(expected)<<"\n";
     }
-    vector<float> output = {0.0f, 0.0f, 0.0f, 0.0f, 1.f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,0.0f, 0.0f,};
     return 0;
 }
